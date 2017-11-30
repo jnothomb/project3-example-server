07147652edbdef75f3cf57544da2ed3e11b78b61
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-30 17:57:53 +0100

    updated the routes.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const flash = require("connect-flash");
 const session = require("express-session");
 const mongoose = require("mongoose");
 const MongoStore = require("connect-mongo")(session);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require("./routes/index");
 
-var app = express();
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI, {
@@ -21,11 +23,11 @@ mongoose.connect(process.env.MONGODB_URI, {
   useMongoClient: true
 });
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Session
 app.use(session({
@@ -37,7 +39,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   cookie: {
-    maxAge: 24 * 60 * 60 // 1 day
+    maxAge: 24 * 60 * 60 * 1000
   }
 }));
 
@@ -47,25 +49,24 @@ const passport = require("./helpers/passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', index);
-app.use('/users', users);
+app.use("/", index);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
