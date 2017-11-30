8801ab2970f60387e2c6f92e2e7fdd3edc84be1a
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-30 18:00:30 +0100

    updated the error handeling.

--- app.js
+++ app.js
@@ -51,22 +51,24 @@ app.use(passport.session());
 
 app.use("/", index);
 
-// catch 404 and forward to error handler
+// -- 404 and error handler
+
+// NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+  res.status(404);
+  res.render("not-found");
 });
 
-// error handler
+// NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // always log the error
+  console.error("ERROR", req.method, req.path, err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render("error");
+  }
 });
 
 module.exports = app;
